339538f914ee1c3339e911ed72ec641148946b8e
decarli <benoitdecarli77@gmail.com>
2024-04-10 21:32:21 +0200

    swagger cours

--- back/cmd/main.go
+++ back/cmd/main.go
@@ -1,19 +1,68 @@
 package main
 
 import (
-	"easynight/internal/db"
-	"easynight/internal/routes"
-	"easynight/pkg/utils"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/zc2638/swag"
+	"github.com/zc2638/swag/api"
 
 	"github.com/labstack/echo/v4"
 )
 
+type Category struct {
+	ID     int64  `json:"category"`
+	Name   string `json:"name" enum:"dog,cat" required:""`
+	Exists *bool  `json:"exists" required:""`
+}
+
+// Pet example from the swagger pet store
+type Pet struct {
+	ID        int64     `json:"id"`
+	Category  *Category `json:"category" desc:"分类"`
+	Name      string    `json:"name" required:"" example:"张三" desc:"名称"`
+	PhotoUrls []string  `json:"photoUrls"`
+	Tags      []string  `json:"tags" desc:"标签"`
+}
+
+func handle(w http.ResponseWriter, r *http.Request) {
+	_, _ = io.WriteString(w, fmt.Sprintf("[%s] Hello World!", r.Method))
+}
+
 func main() {
-	e := echo.New()
+	router := echo.New()
 
-	db.DatabaseInit()
+	api.Walk(func(path string, e *swag.Endpoint) {
+		h := echo.WrapHandler(e.Handler.(http.Handler))
+		path = swag.ColonPath(path)
 
-	routes.InitRouter(e)
+		switch strings.ToLower(e.Method) {
+		case "get":
+			router.GET(path, h)
+		case "head":
+			router.HEAD(path, h)
+		case "options":
+			router.OPTIONS(path, h)
+		case "delete":
+			router.DELETE(path, h)
+		case "put":
+			router.PUT(path, h)
+		case "post":
+			router.POST(path, h)
+		case "trace":
+			router.TRACE(path, h)
+		case "patch":
+			router.PATCH(path, h)
+		case "connect":
+			router.CONNECT(path, h)
+		}
+	})
 
-	e.Logger.Fatal(e.Start(":" + utils.GetEnvVariable("PORT")))
+	router.GET("/swagger/json", echo.WrapHandler(swag.HandlerWrap()))
+	router.GET("/swagger/ui/*", echo.WrapHandler(swag.UIHandler("/swagger/ui", "/swagger/json", true)))
+
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
--- back/go.mod
+++ back/go.mod
@@ -11,6 +11,7 @@ require (
 )
 
 require (
+	github.com/99nil/gopkg v0.0.0-20220607055250-e19b23d7661a // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/pgx/v5 v5.4.3 // indirect
@@ -19,8 +20,10 @@ require (
 	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
+	github.com/zc2638/swag v1.14.0 // indirect
 	golang.org/x/crypto v0.17.0 // indirect
 	golang.org/x/net v0.19.0 // indirect
 	golang.org/x/sys v0.15.0 // indirect
