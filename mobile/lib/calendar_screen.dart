64902a3a27cd8620fc42837931e8d7b21adee24c
RSelwa <selwa.raphael@gmail.com>
2024-04-11 12:27:42 +0200

    exercice

---
+++ mobile/lib/calendar_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class CalendarScreen extends StatelessWidget {
+  const CalendarScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(child: Text('Calendar')),
+    );}
+}
--- mobile/lib/home_screen.dart
+++ mobile/lib/home_screen.dart
@@ -1,31 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:mobile/calendar_screen.dart';
+import 'package:mobile/favorite_screen.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final screens = [FavoriteScreen(), CalendarScreen()];
+  int _currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar:
-      PreferredSize(
-        preferredSize: const Size.fromHeight(100),
-        child: Container(child:const Text('eerte' )),
-      ),
-      bottomNavigationBar: Container(child:const Text("test")) ,
-      body: Container(
-        color: Colors.white,
-        child: const Center(
-          child: Directionality(textDirection: TextDirection.ltr,child: Text(
-            "EASY NIGHT",
-            style: TextStyle(color:Colors.red, fontSize: 22, fontWeight: FontWeight.bold),
-          )),
+        backgroundColor: Colors.white,
+        bottomNavigationBar: BottomNavigationBar(
+          onTap: (index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+          items: [
+            const BottomNavigationBarItem(
+                icon: Icon(Icons.favorite), label: 'Favorites'),
+            const BottomNavigationBarItem(
+                icon: Icon(Icons.calendar_today), label: "Calendar")
+          ],
         ),
-      ),
-    );
+        body: screens[_currentIndex]);
   }
 }
-
 
 class AppBar extends StatelessWidget {
   const AppBar({super.key});
