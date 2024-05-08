c45802218cfe5e2f5727139b673163ebd19ace12
Nicolas CAPELLA <colas.capel73@gmail.com>
2024-05-08 15:07:41 +0200

    :sparkles: Translation

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:mobile/theme_data.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:mobile/layout.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
 
 void main() async {
   await dotenv.load();
@@ -20,6 +23,16 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: easyTheme,
       themeMode: ThemeMode.dark,
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('en'),
+        Locale('fr'),
+      ],
     );
   }
 }
