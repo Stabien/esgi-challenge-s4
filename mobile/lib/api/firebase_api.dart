4fdd2d7e3e43bde617f3d9026721f637dab66dd6
Nicolas CAPELLA <colas.capel73@gmail.com>
2024-05-09 12:14:40 +0200

    :sparkles: Notification with firebase

---
+++ mobile/lib/api/firebase_api.dart
@@ -0,0 +1,16 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+
+class FirebaseApi {
+  // Create an instance of Firebase Messaging
+  static final FirebaseMessaging _firebaseMessaging =
+      FirebaseMessaging.instance;
+
+  // function to initialize notifications
+  Future<void> initNotifications() async {
+    await _firebaseMessaging.requestPermission();
+
+    final token = await _firebaseMessaging.getToken();
+
+    print('Token: $token');
+  }
+}
--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,15 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:mobile/api/firebase_api.dart';
 import 'package:mobile/theme_data.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:mobile/layout.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   await dotenv.load();
   await dotenv.load(fileName: ".env.local");
   runApp(const MyApp());
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  await FirebaseApi().initNotifications();
 }
 
 class MyApp extends StatelessWidget {
