c613c2367d8e2bd68fc7db01865cc0742ef96ed7
Stabien <bastien.piedallu@gmail.com>
2024-06-15 18:00:16 +0200

    fix(ops): fix ssh key

--- back/terraform/scripts/install.sh
+++ back/terraform/scripts/install.sh
@@ -25,3 +25,9 @@ sudo mv /tmp/id_rsa ~/.ssh/id_rsa
 sudo chmod 600 ~/.ssh/id_rsa
 
 ssh-keyscan github.com >> ~/.ssh/known_hosts
+
+sudo chmod 777 /home
+sudo yes yes | git clone --branch feat/add-cd git@github.com:Stabien/esgi-challenge-s4
+
+cd esgi-challenge-s4/back
+sudo cp /tmp/.env.local ./.env.local
