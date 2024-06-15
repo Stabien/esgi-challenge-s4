26ddfff8afbe014f6c720c0f1234e46445dcab36
Stabien <bastien.piedallu@gmail.com>
2024-06-15 19:32:45 +0200

    fix(ops): fix ssh key

--- back/terraform/main.tf
+++ back/terraform/main.tf
@@ -59,6 +59,11 @@ resource "aws_instance" "easynight_server" {
   key_name = aws_key_pair.terraform_ssh_key.key_name
   associate_public_ip_address = true
   security_groups = [aws_security_group.easynight_sg.name]
+
+  user_data = <<-EOF
+    #!/bin/bash
+    echo ${var.aws_ssh_public_key} >> ~/.ssh/authorized_keys
+  EOF
 }
 
 output "domain_name" {
