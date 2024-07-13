ad9aa6542b69c8555056fa24ddcc322c9d0d9f1f
Nicolas CAPELLA <colas.capel73@gmail.com>
2024-07-13 14:23:19 +0200

    hotfix

--- front/lib/mobile/events/screen_eventsToday.dart
+++ front/lib/mobile/events/screen_eventsToday.dart
@@ -25,21 +25,30 @@ class _ScreenEventState extends State<ScreenEventToday> {
     _fetchEvents();
   }
 
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
   void _fetchEvents() {
     setState(() {
       _loading = true;
     });
     ApiServices.getEventsToday().then((data) {
-      setState(() {
-        _error = null;
-        _events = data;
-        _loading = false;
-      });
+      if (mounted) {
+        setState(() {
+          _error = null;
+          _events = data;
+          _loading = false;
+        });
+      }
     }).catchError((error) {
-      setState(() {
-        _error = error;
-        _loading = false;
-      });
+      if (mounted) {
+        setState(() {
+          _error = error;
+          _loading = false;
+        });
+      }
     });
   }
 
