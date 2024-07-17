dccb6fe3d9816cabdb0a09407e71d1595ebad455
Stabien <bastien.piedallu@gmail.com>
2024-07-17 15:52:01 +0200

    fix(back): fix fixtures

--- back/internal/fixtures/organizerFixtures.go
+++ back/internal/fixtures/organizerFixtures.go
@@ -2,12 +2,14 @@ package fixtures
 
 import (
 	"easynight/internal/models"
+
+	"github.com/google/uuid"
 )
 
 var Organizers = []models.Organizer{
-	{UserID: BaseOrganizers[0].ID, User: BaseOrganizers[0], Firstname: "Organizer", Lastname: "Test"},
-	{UserID: BaseOrganizers[1].ID, User: BaseOrganizers[1], Firstname: "David", Lastname: "Guetta"},
-	{UserID: BaseOrganizers[2].ID, User: BaseOrganizers[2], Firstname: "DJ", Lastname: "Snake"},
-	{UserID: BaseOrganizers[3].ID, User: BaseOrganizers[3], Firstname: "Café", Lastname: "Oz"},
-	{UserID: BaseOrganizers[4].ID, User: BaseOrganizers[4], Firstname: "La", Lastname: "Clairière"},
+	{ID: uuid.New(), UserID: BaseOrganizers[0].ID, User: BaseOrganizers[0], Firstname: "Organizer", Lastname: "Test"},
+	{ID: uuid.New(), UserID: BaseOrganizers[1].ID, User: BaseOrganizers[1], Firstname: "David", Lastname: "Guetta"},
+	{ID: uuid.New(), UserID: BaseOrganizers[2].ID, User: BaseOrganizers[2], Firstname: "DJ", Lastname: "Snake"},
+	{ID: uuid.New(), UserID: BaseOrganizers[3].ID, User: BaseOrganizers[3], Firstname: "Café", Lastname: "Oz"},
+	{ID: uuid.New(), UserID: BaseOrganizers[4].ID, User: BaseOrganizers[4], Firstname: "La", Lastname: "Clairière"},
 }
--- back/internal/fixtures/userFixtures.go
+++ back/internal/fixtures/userFixtures.go
@@ -18,21 +18,21 @@ func getDefaultPassword() string {
 	return password
 }
 
-var BaseAdmins = []models.User{{ID: uuid.New(), Email: "admin@test.fr", Password: getDefaultPassword(), Role: "admin"}}
+var BaseAdmins = []models.User{{ID: uuid.New(), Email: "admin@test.fr", Password: getDefaultPassword(), Role: "admin", Token: ""}}
 var BaseCustomers = []models.User{
-	{ID: uuid.New(), Email: "customer@test.fr", Password: getDefaultPassword(), Role: "customer"},
-	{ID: uuid.New(), Email: "antoine.dupont@test.fr", Password: getDefaultPassword(), Role: "customer"},
-	{ID: uuid.New(), Email: "gael.fickou@test.fr", Password: getDefaultPassword(), Role: "customer"},
-	{ID: uuid.New(), Email: "damien.penaud@test.fr", Password: getDefaultPassword(), Role: "customer"},
-	{ID: uuid.New(), Email: "romain.ntamack@test.fr", Password: getDefaultPassword(), Role: "customer"},
+	{ID: uuid.New(), Email: "customer@test.fr", Password: getDefaultPassword(), Role: "customer", Token: ""},
+	{ID: uuid.New(), Email: "antoine.dupont@test.fr", Password: getDefaultPassword(), Role: "customer", Token: ""},
+	{ID: uuid.New(), Email: "gael.fickou@test.fr", Password: getDefaultPassword(), Role: "customer", Token: ""},
+	{ID: uuid.New(), Email: "damien.penaud@test.fr", Password: getDefaultPassword(), Role: "customer", Token: ""},
+	{ID: uuid.New(), Email: "romain.ntamack@test.fr", Password: getDefaultPassword(), Role: "customer", Token: ""},
 }
 
 var BaseOrganizers = []models.User{
-	{ID: uuid.New(), Email: "organizer@test.fr", Password: getDefaultPassword(), Role: "organizer"},
-	{ID: uuid.New(), Email: "david.guetta@test.fr", Password: getDefaultPassword(), Role: "organizer"},
-	{ID: uuid.New(), Email: "dj.snake@test.fr", Password: getDefaultPassword(), Role: "organizer"},
-	{ID: uuid.New(), Email: "cafe.oz@test.fr", Password: getDefaultPassword(), Role: "organizer"},
-	{ID: uuid.New(), Email: "la.clairiere@test.fr", Password: getDefaultPassword(), Role: "organizer"},
+	{ID: uuid.New(), Email: "organizer@test.fr", Password: getDefaultPassword(), Role: "organizer", Token: ""},
+	{ID: uuid.New(), Email: "david.guetta@test.fr", Password: getDefaultPassword(), Role: "organizer", Token: ""},
+	{ID: uuid.New(), Email: "dj.snake@test.fr", Password: getDefaultPassword(), Role: "organizer", Token: ""},
+	{ID: uuid.New(), Email: "cafe.oz@test.fr", Password: getDefaultPassword(), Role: "organizer", Token: ""},
+	{ID: uuid.New(), Email: "la.clairiere@test.fr", Password: getDefaultPassword(), Role: "organizer", Token: ""},
 }
 
 var Users = utils.MergeSlices(BaseAdmins, BaseCustomers, BaseOrganizers)
