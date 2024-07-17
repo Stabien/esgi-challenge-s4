91660b400f6080e598c3ef7f388a1863abcf103c
Benoit <benoitdecarli77@gmail.com>
2024-07-17 09:42:31 +0200

    felche qr code

--- front/lib/mobile/screens/qr_code_scanner_screen.dart
+++ front/lib/mobile/screens/qr_code_scanner_screen.dart
@@ -37,7 +37,10 @@ class _QRCodeScannerScreenState extends State<QRCodeScannerScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Scanner QR Code')),
+      appBar: AppBar(
+        title: Text('Scanner QR Code'),
+        automaticallyImplyLeading: false,
+      ),
       body: Stack(
         children: <Widget>[
           Column(
